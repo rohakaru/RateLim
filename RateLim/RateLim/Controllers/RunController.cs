0730841a8bd03447bf2fb142467a1776d3677228
Heng Lu <rohakaru@gmail.com>
2021-10-13 19:43:16 +0800

    新增Run API，預設輸出Status200

---
+++ RateLim/RateLim/Controllers/RunController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RateLim.Controllers
+{
+    [Route("v1/[controller]")]
+    [ApiController]
+    public class RunController : ControllerBase
+    {
+        // GET: v1/<RunController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
